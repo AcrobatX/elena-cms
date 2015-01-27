7fd2833fb80d8b1d4287170516faf1602955520b
Bojan Rogulja <bojan.rogulja@gmail.com>
2015-01-27 22:54:30 +0100

    Initial PHP

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+///error_reporting(E_ALL);
+//error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+error_reporting(E_ERROR | E_WARNING | E_PARSE );
+
+///definition of files and folders
+define('DS', DIRECTORY_SEPARATOR);
+//define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('ROOT', __DIR__);
+define('ROOT_CORE', ROOT.DS.".elena".DS);
+define('ROOT_CORE_SETTINGS', ROOT_CORE."settings".DS);
+define('ROOT_MODULES', ROOT.DS."modules".DS);
+define('ROOT_THEMES', ROOT.DS."themes".DS);
+///Include part
+include_once( ROOT_CORE."controller.php");
+//instantiate an object of WebPage class is inside controller
+$page = new WebPage();
+
+
+//die ();
+/*Include part*/
+
+
+    $out = "<pre>";
+    foreach($_SERVER as $key => $value )
+    {
+       $out .= "$key => $value"."\r\n|----------------------------------------> <-----------------------------------\r\n";
+    }
+    $out .= "</pre>";
+    echo $out;
+
+?>
